b76177550a8e0d79e03cbd8c1ee2006aca60d909
Viktorpalffy <55533463+Viktorpalffy@users.noreply.github.com>
2019-09-20 09:22:14 +0200

    Add files via upload

---
+++ szamcsere.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int szam1,szam2;
+    cout<<"Kerek ket szamot: "<<endl;
+    cin>>szam2;
+    cin>>szam1;
+    cout<<"A ket szam felcserelve: "<<szam1<<' '<<szam2<<endl;
+
+    return 0;
+}
