d3479b3d9e32de833af626f6e22e6836ca51dbd2
Viktorpalffy <55533463+Viktorpalffy@users.noreply.github.com>
2019-10-03 08:29:01 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int a=6;
+    int b=9;
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    cout<<a<<" "<<b<<endl;
+    return 0;
+}
